b5c835033a43709453c15cd47e4fb148c808169e
Kwabena Boakye <kwaboakye93@gmail.com>
2023-05-11 07:26:37 +0000

    Init Script

---
+++ models/engine/__init__.py
@@ -0,0 +1,17 @@
+"""
+File Storage
+==========
+
+This is a Python module that is responsible for all
+tests in the AirBnB clone.
+
+Submodules:
+- file_storage: module.
+
+
+Classes:
+- FileStorage: The class in file_storage module.
+
+Functions:
+- None
+"""
