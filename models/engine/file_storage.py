0e35c090b153a59a8cb99c39ea62481a2875e999
Kwabena Boakye <kwaboakye93@gmail.com>
2023-05-16 11:00:58 +0000

    Change variable '__file_name' and 'object'

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,13 +22,13 @@ class FileStorage:
     """
     File Storage Engine
         Class Attrs:
-            __file_name(str) -> JSON filename
-            __instances(dict) -> keeps all instances
+            __file_path(str) -> JSON filename
+            __objects(dict) -> keeps all instances
             based in the id of its class
             models(dict) -> Inventory of all models
     """
-    __file_name = 'file.json'
-    __instances: dict = {}
+    __file_path = 'file.json'
+    __objects: dict = {}
     models: dict = {
         'BaseModel': BaseModel,
         'Amenity': Amenity,
@@ -42,39 +42,39 @@ class FileStorage:
 
     def new(self, obj):
         """
-        sets in __instances the obj
+        sets in __objects the obj
         with its <class name>.id as its key
         """
 
         key = f'{obj.__class__.__name__}.{str(obj.id)}'
-        self.__class__.__instances[key] = obj
+        self.__class__.__objects[key] = obj
 
     def all(self):
-        """Return the elements in __instances dictionary
+        """Return the elements in __objects dictionary
         """
-        return (self.__class__.__instances)
+        return (self.__class__.__objects)
 
     def save(self):
         """
-        serializes __instances dict into a JSON file
-        (path: __file_name)
+        serializes __objects dict into a JSON file
+        (path: __file_path)
         """
         objects_dict = {}
-        for key, value in self.__class__.__instances.items():
+        for key, value in self.__class__.__objects.items():
             objects_dict[key] = value.to_dict()
 
-        with open(FileStorage.__file_name, 'w', encoding='UTF-8') as storage:
+        with open(FileStorage.__file_path, 'w', encoding='UTF-8') as storage:
             json.dump(objects_dict, storage, indent=4)
 
     def reload(self):
         """
-        deserializes the JSON file into __instances dict
-        (only if the JSON file (ie __file_name) exists)
+        deserializes the JSON file into __objects dict
+        (only if the JSON file (ie __file_path) exists)
         does nothing otherwise
         """
-        if os.path.exists(FileStorage.__file_name):
-            with open(FileStorage.__file_name, 'r', encoding='UTF-8') as file:
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r', encoding='UTF-8') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     base = FileStorage.models[value['__class__']](**value)
-                    FileStorage.__instances[key] = base
+                    FileStorage.__objects[key] = base
