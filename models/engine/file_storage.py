9864701482fbc812fffb66820ba2f353a56163a2
Peter Boakye <112291489+petboa@users.noreply.github.com>
2023-05-21 21:59:49 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,14 +67,13 @@ class FileStorage:
             json.dump(objects_dict, f)
 
     def reload(self):
-        """
-        deserializes the JSON file into __objects dict
-        (only if the JSON file (ie __file_path) exists)
-        does nothing otherwise
-        """
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r', encoding='UTF-8') as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    base = FileStorage.models[value['__class__']](**value)
-                    FileStorage.__objects[key] = base
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
