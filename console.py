d6068910f41a5315eb75c5990a19f50d4b5e1a7d
Peter Boakye <112291489+petboa@users.noreply.github.com>
2023-05-21 20:33:59 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,104 +1,153 @@
-#!/usr/bin/python3
-"""Module for the entry point of the command interpreter."""
+#!/usr/bin/env python3
+"""
+Hbnb Console
+==================
+This module is a Command Line Interpretor for an AirBnb Clone
+
+Example Usage:
+----------------
+
+
+Class:
+-----
+
+"""
 
 import cmd
 from models.base_model import BaseModel
-from models import storage
-import re
-import json
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.reviews import Review
 
 
-class MyCommand(cmd.Cmd):
-    """Class for the command interpreter."""
+class HBNBCommand(cmd.Cmd):
+    intro = 'Welcome to the AirBnB console. Type help or ? to list commands.\n'
+    prompt = '(malik)'
+    file = None
 
-    prompt = "(hbnb) "
-
-    def default(self, line):
-        """Catch commands if nothing else matches."""
-        self._precmd(line)
-
-    def _precmd(self, line):
-        """Intercept commands to test for class.syntax()"""
-        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
-        if not match:
-            return line
-        classname = match.group(1)
-        method = match.group(2)
-        args = match.group(3)
-        match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', args)
-        if match_uid_and_args:
-            uid = match_uid_and_args.group(1)
-            attr_or_dict = match_uid_and_args.group(2)
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id."""
+        if arg == '':
+            print('** class name missing **')
+        elif arg != 'BaseModel':
+            print('** class doesn\'t exist **')
         else:
-            uid = args
-            attr_or_dict = False
+            args = arg.split("")
+            obj = eval(args[0])()
+            obj.save()
+            print(obj.id)
 
-        attr_and_value = ""
-        if method == "update" and attr_or_dict:
-            match_dict = re.search('^({.*})$', attr_or_dict)
-            if match_dict:
-                self.update_dict(classname, uid, match_dict.group(1))
-                return ""
-            match_attr_and_value = re.search(
-                '^(?:"([^"]*)")?(?:, (.*))?$', attr_or_dict)
-            if match_attr_and_value:
-                attr_and_value = (match_attr_and_value.group(
-                    1) or "") + " " + (match_attr_and_value.group(2) or "")
-        command = method + " " + classname + " " + uid + " " + attr_and_value
-        self.onecmd(command)
-        return command
-
-    def update_dict(self, classname, uid, s_dict):
-        """Helper method for update() with a dictionary."""
-        s = s_dict.replace("'", '"')
-        d = json.loads(s)
-        if not classname:
-            print("** class name missing **")
-        elif classname not in storage.classes():
-            print("** class doesn't exist **")
-        elif uid is None:
-            print("** instance id missing **")
+    def do_show(self, arg):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+        if arg == '':
+            print('** class name missing **')
+        elif arg != 'BaseModel':
+            print('** class doesn\'t exist **')
+        elif arg.find(' ') == -1:
+            print('** instance id missing **')
+        elif arg.id != 'BaseModel':
+            print('** no instance found **')
         else:
-            key = "{}.{}".format(classname, uid)
-            if key not in storage.all():
-                print("** no instance found **")
-            else:
-                attributes = storage.attributes()[classname]
-                for attribute, value in d.items():
-                    if attribute in attributes:
-                        value = attributes[attribute](value)
-                    setattr(storage.all()[key], attribute, value)
-                storage.all()[key].save()
+            obj = BaseModel()
+            obj.__str__()
+            print(obj)
 
-    def do_EOF(self, line):
-        """Handle End Of File character."""
-        print()
-        return True
+    def do_eof(self, arg):
+        'Exit the console'
+        print('Thank you for using AirBnB')
+        return -1
 
-    def do_quit(self, line):
-        """Exit the program."""
-        return True
+    def do_all(self, arg):
+        """Prints all string representation of all
+        instances based or not on the class name"""
+        if arg == '':
+            print('** class name missing **')
+        elif arg != 'BaseModel':
+            print('** class doesn\'t exist **')
+        else:
+            obj = BaseModel()
+            obj.__str__()
+            for i in obj:
+                print(i)
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name
+        and id (save the change into the JSON file)."""
+        if arg == '':
+            print('** class name missing **')
+        elif arg != 'BaseModel':
+            print('** class doesn\'t exist **')
+        elif arg.find(' ') == -1:
+            print('** instance id missing **')
+        elif arg.id != 'BaseModel':
+            print('** no instance found **')
+        else:
+            obj = BaseModel()
+            obj.delete()
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and
+        id by adding or updating attribute"""
+
+        if arg == '':
+            print('** class name missing **')
+        elif arg != 'BaseModel':
+            print('** class doesn\'t exist **')
+        elif arg.find(' ') == -1:
+            print('** instance id missing **')
+        elif arg.id != 'BaseModel':
+            print('** no instance found **')
+        elif arg.find(' ') == -1:
+            print('** attribute name missing **')
+        elif arg.find(' ') == -1:
+            print('** value missing **')
+        else:
+            arg = arg.split()
+            arg[1] = arg[1].replace('"', '')
+            arg[2] = arg[2].replace('"', '')
+            obj = BaseModel()
+            obj.__dict__[arg[1]] = arg[2]
+            obj.save()
+            obj.__str__()
+            print(obj)
+
+    def do_quit(self, arg):
+        'Prints a farewell message and exits the console'
+        print('Thank you for using AirBnB')
+        return -1
+
+    def precmd(self, line):
+        line = line.lower()
+        if self.file and 'playback' not in line:
+            print(line, file=self.file)
+        return line
+
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
 
     def emptyline(self):
-        """Do nothing on ENTER."""
         pass
 
-    def do_create(self, line):
-        """Create an instance."""
-        if line == "" or line is None:
-            print("** class name missing **")
-        elif line not in storage.classes():
-            print("** class doesn't exist **")
-        else:
-            b = storage.classes()[line]()
-            b.save()
-            print(b.id)
+    def do_help(self, arg):
+        'List available commands with "help" or detailed help with "help cmd".'
+        cmd.Cmd.do_help(self, arg)
 
-    def do_show(self, line):
-        """Print the string representation of an instance."""
-        if line == "" or line is None:
-            print("** class name missing **")
-        else:
-            words = line.split(' ')
-            if words[0] not in storage.classes():
-                print("** class doesn't exist **
+    def default(self, line):
+        'Called on an input line when the command prefix is not recognized.'
+        print('*** Unknown syntax: {}'.format(line))
+
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
